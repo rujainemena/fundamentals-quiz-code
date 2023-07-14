e7c39e0a1e5b0129c1f9c45cc25ff82828e3c83f
rujainemena <135552275+rujainemena@users.noreply.github.com>
2023-07-13 21:44:52 -0400

    add functions to start quiz

---
+++ Assets/JS/script.js
@@ -0,0 +1,87 @@
+var welcomeEl = document.querySelector("#intro-section");
+var startBtn = document.querySelector("#start-btn");
+var timerEl = document.querySelector("#timer");
+var questionsEl = document.querySelector("#questions");
+var choicesEl = document.querySelectorAll(".choices");
+var titleEL = document.querySelector("#title");
+// var initialEl = document.querySelector("#initials");
+// var highScore = document.querySelector("#highscore");
+// var score = document.querySelector("#score")
+// var backBtn = document.querySelector("#back-btn");
+// var clearBtn = document.querySelector("#clear-btn");
+
+
+// data structure to store questions and choices
+var questionIndex = 0;
+var questionGroup = [
+    {
+        title: "Q1",
+        choices: ["c1", "c2", "c3", "c4"],
+        answer: "c2"
+    },
+    {
+        title: "Q2",
+        choices: ["c1", "c2", "c3", "c4"],
+        answer: "c3"
+    },
+    {
+        title: "Q3",
+        choices: ["c1", "c2", "c3", "c4"],
+        answer: "c4",
+    },
+    {
+        title: "Q4",
+        choices: ["c1", "c2", "c3", "c4"],
+        answer: "c1"
+    },
+    {
+        title: "Q5",
+        choices: ["c1", "c2", "c3", "c4"],
+        answer: "c3"
+    }
+];
+
+
+// Function starts quiz, hides welcome page when the start button is clicked, and starts the timer.
+function startQuiz() {
+    welcomeEl.classList.add("hide"); //hides welcome page
+    questionsEl.removeAttribute("class"); //displays next page
+    setIntervalId = setInterval(countdown, 1000); //turns on timer
+    showQuest(); //calls dynamic questions
+}
+
+var setIntervalId = 0;
+var timeLeft = questionGroup.length * 15;
+// timer countdown as the quiz begins
+function countdown() {
+    timerEl.textContent = timeLeft--
+    if (timeLeft === 0) {
+        clearInterval(setIntervalId);
+    }
+}
+
+
+// dynamic function to display quiz questions.
+function showQuest() {
+    titleEL.textContent = questionGroup[questionIndex].title;
+    choicesEl[0].textContent = questionGroup[questionIndex].choices[0];
+    choicesEl[1].textContent = questionGroup[questionIndex].choices[1];
+    choicesEl[2].textContent = questionGroup[questionIndex].choices[2];
+    choicesEl[3].textContent = questionGroup[questionIndex].choices[3];
+}
+
+// Function to proceed to the following question after answer is selected.
+function nextQuestion(event){
+    var currentElement = event.target;
+    if (currentElement.matches("button")) {
+        questionIndex++
+        showQuest();
+    }
+
+    
+}
+
+
+// User actions
+startBtn.addEventListener("click", startQuiz);
+questionsEl.addEventListener("click", nextQuestion);
